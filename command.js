db50a0294d5f737363d8dc5446e452fd2d426a8b
kevinonyoh <evionyoh@gmail.com>
2024-03-22 14:44:39 +0100

    testing sending bot image2

--- command.js
+++ command.js
@@ -12,7 +12,20 @@ function command(chatId,data){
             
           formData.append('chat_id', chatId);
           formData.append('photo', fs.createReadStream('./download.png'));
-          formData.append('caption', "Hello, there, welcome to TWT token airdrop Bot");
+          formData.append('caption', 
+              `Hello, Hsaka_trade 👋 , I am your friendly TWT  Airdrop bot
+
+              🔹 Earn 9,000 TWT (~$ 11,970) For Completing Tasks
+              🔹 Earn 100 TWT (~$ 133) For Each Refer
+              
+              📢 Airdrop Rules
+              
+              ✏️ Mandatory Tasks :
+              🔹 Join our Telegram Group 
+              🔹 Join our Telegram Channel 
+              🔹 Refer at least 5 Friends
+              
+              Click On  "✅ Join Airdrop" For Procced`);
 
         return {
           formData,
